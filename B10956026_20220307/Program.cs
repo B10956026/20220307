b97168f68d84d4318983c1b6f35cd383e7e1fc1b
user <user@DESKTOP-6JBEF5K>
2022-03-07 11:12:53 +0800

    加入專案檔案。

---
+++ B10956026_20220307/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B10956026_20220307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
