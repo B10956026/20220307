c4f12d8189dc1d1d4a2e252f73c329c51a95b3bf
user <user@DESKTOP-6JBEF5K>
2022-03-07 11:51:11 +0800

    練習1

--- B10956026_20220307/Program.cs
+++ B10956026_20220307/Program.cs
@@ -10,7 +10,13 @@ namespace B10956026_20220307
     {
         static void Main(string[] args)
         {
+            double meal_cost = 100; //用餐費用
+            int tip_percent = 15; //服務費
+            int tax_percent = 8; //稅率
 
+            int sum = tip_percent + tax_percent;
+
+            Console.WriteLine(sum);
         }
     }
 }
